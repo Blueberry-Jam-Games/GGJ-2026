81f96306d20fa51f92c4dfb9cb72b612ffddfa74
Cyb3rGam3rX <quinnrichardson91@gmail.com>
2026-01-30 18:13:45 -0800

    Added build code

---
+++ Assets/Scripts/SmokeTest.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SmokeTest
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeInitialized()
+    {
+        Debug.Log("Unity Build Ready");
+    }
+}
