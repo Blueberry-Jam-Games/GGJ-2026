efb2e58fae9152c3615cc7d1d7edf7c1a44f19f2
Cyb3rGam3rX <quinnrichardson91@gmail.com>
2026-01-31 13:59:30 -0800

    Backup before update

---
+++ Assets/Scripts/Dialogue/Playback/DialogueManager.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshPro dialogueBox;
+
+    public void startDialogue(Dialogue dialogue)
+    {
+        
+    }
+}
